6a59b2c793fc585c775260f7ba4744bb64188c03
Bartosz Wiśniewski <kontakt@pr0gramista.pl>
2022-02-20 14:47:51 +0100

    Make example more usable and add new test cases

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,73 +1,42 @@
 import 'dart:developer';
 import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:charset_converter/charset_converter.dart';
+import 'package:flutter/services.dart';
+import 'package:super_form/super_form.dart';
 
 void main() => runApp(const MyApp());
 
+const gap = SizedBox(
+  width: 8,
+  height: 8,
+);
+
+const horizontalPadding = EdgeInsets.symmetric(horizontal: 8);
+
+enum ConversionMode { encode, decode, checkAvailability }
+
 class MyApp extends StatefulWidget {
-  const MyApp({Key key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  String _decoded;
-  Uint8List _encoded;
-  bool _errored = false;
+  String? output;
+  bool isLoading = false;
+  ConversionMode mode = ConversionMode.encode;
 
-  @override
-  void initState() {
-    super.initState();
-    decodeAndEncode();
-  }
+  Uint8List _parseByteList(String input) {
+    final parts = input.split(',');
+    final parsedBytes = parts.map((e) => int.tryParse(e)).toList();
 
-  Future<void> decodeAndEncode() async {
-    String decoded;
-    Uint8List encoded;
-
-    try {
-      encoded =
-          await CharsetConverter.encode("TIS620", "สวัสดี"); // Hello in Thai
-      decoded = await CharsetConverter.decode(
-          "windows1250",
-          Uint8List.fromList(
-              [0x43, 0x7A, 0x65, 0x9C, 0xE6])); // Hello in Polish
-    } on PlatformException catch (e) {
-      log(e.toString());
-      setState(() {
-        _errored = true;
-      });
-      return;
+    if (parsedBytes.contains(null)) {
+      throw FormatException("Could not parse $input as list of bytes");
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _decoded = decoded;
-      _encoded = encoded;
-    });
-  }
-
-  /// Generates widget builder for [FutureBuilder] for given [charset]
-  Function _charsetAvailabilityView(String charset) {
-    return (context, snapshot) {
-      if (snapshot.hasData) {
-        bool isAvailable = snapshot.data as bool;
-
-        return Text("Is $charset available: " + isAvailable.toString(),
-            key: Key('charset_$charset'));
-      }
-      return const CircularProgressIndicator();
-    };
+    return Uint8List.fromList(parsedBytes.cast<int>());
   }
 
   @override
@@ -77,41 +46,176 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('CharsetConverter example'),
         ),
-        body: Center(
-            child: Column(children: [
-          if (_decoded != null)
-            Text("Decoded from Windows CP1250: " + _decoded,
-                key: const Key('decoded')),
-          if (_encoded != null)
-            Text("Encoded to TIS620: " + _encoded.join(", "),
-                key: const Key('encoded')),
-          if (_errored) const Text("Something went wrong :C"),
-          FutureBuilder(
-              future: CharsetConverter.checkAvailability(
-                  "i_should_not_be_available"),
-              builder: _charsetAvailabilityView("i_should_not_be_available")),
-          FutureBuilder(
-              future: CharsetConverter.checkAvailability("EUC-KR"),
-              builder: _charsetAvailabilityView("EUC-KR")),
-          Expanded(
-              child: FutureBuilder(
-            future: CharsetConverter.availableCharsets(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                final charsets = snapshot.data as List<String>;
+        body: Builder(builder: (context) {
+          return SuperForm(
+            onSubmit: (values) async {
+              final String? input = values["input"];
+              final String charset = values["charset"];
 
-                return ListView.builder(
-                    key: const Key("available_charsets"),
-                    itemCount: charsets.length,
-                    itemBuilder: (context, index) => Text(charsets[index]));
+              setState(() {
+                output = null;
+                isLoading = true;
+              });
+
+              try {
+                if (mode == ConversionMode.encode) {
+                  final result = await CharsetConverter.encode(charset, input!);
+
+                  setState(() {
+                    output = result.join(', ');
+                  });
+                } else if (mode == ConversionMode.decode) {
+                  final result = await CharsetConverter.decode(
+                      charset, _parseByteList(input!));
+
+                  setState(() {
+                    output = result;
+                  });
+                } else if (mode == ConversionMode.checkAvailability) {
+                  final result =
+                      await CharsetConverter.checkAvailability(charset);
+
+                  setState(() {
+                    output = result ? "Available" : "Not found";
+                  });
+                } else {
+                  throw UnimplementedError("Unknown conversion mode");
+                }
+              } on PlatformException catch (e) {
+                log(e.toString());
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text("Something went wrong"),
+                  ),
+                );
+              } finally {
+                setState(() {
+                  isLoading = false;
+                });
               }
-              if (snapshot.hasError) {
-                return Text(snapshot.error.toString());
-              }
-              return const CircularProgressIndicator();
             },
-          )),
-        ])),
+            child: ListView(
+              children: [
+                gap,
+                gap,
+                Padding(
+                  padding: horizontalPadding,
+                  child: TextSuperFormField(
+                    name: "input",
+                    rules: [
+                      CustomRule((input) {
+                        if (mode == ConversionMode.encode ||
+                            mode == ConversionMode.decode) {
+                          if (input == null || (input as String?)!.isEmpty) {
+                            return "Cannot be empty";
+                          }
+                        }
+
+                        if (mode == ConversionMode.decode) {
+                          try {
+                            _parseByteList(input);
+                          } on FormatException catch (_) {
+                            return "Must be in format of comma-separated bytes";
+                          }
+                        }
+                        return null;
+                      })
+                    ],
+                    decoration: const InputDecoration(
+                      label: Text("Input"),
+                      hintText: "Text or list of bytes like 65, 66, 67",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                gap,
+                gap,
+                Padding(
+                  padding: horizontalPadding,
+                  child: TextSuperFormField(
+                    name: "charset",
+                    rules: [
+                      RequiredRule("Cannot be empty"),
+                    ],
+                    decoration: const InputDecoration(
+                      label: Text("Charset"),
+                      hintText: "Charset name like windows1250",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                gap,
+                Builder(builder: (context) {
+                  return Row(
+                    children: [
+                      gap,
+                      ElevatedButton(
+                        onPressed: () {
+                          mode = ConversionMode.encode;
+
+                          SuperForm.of(context, listen: false).submit();
+                        },
+                        child: const Text("Encode"),
+                      ),
+                      gap,
+                      ElevatedButton(
+                        onPressed: () {
+                          mode = ConversionMode.decode;
+
+                          SuperForm.of(context, listen: false).submit();
+                        },
+                        child: const Text("Decode"),
+                      ),
+                      gap,
+                      ElevatedButton(
+                        onPressed: () {
+                          mode = ConversionMode.checkAvailability;
+
+                          SuperForm.of(context, listen: false).submit();
+                        },
+                        child: const Text("Check if available"),
+                      ),
+                      gap,
+                      ElevatedButton(
+                        onPressed: () async {
+                          setState(() {
+                            output = null;
+                            isLoading = true;
+                          });
+
+                          final charsets =
+                              await CharsetConverter.availableCharsets();
+
+                          setState(() {
+                            output = charsets.join("\n");
+                            isLoading = false;
+                          });
+                        },
+                        child: const Text("Get available charsets"),
+                      )
+                    ],
+                  );
+                }),
+                gap,
+                if (isLoading)
+                  const Padding(
+                    padding: horizontalPadding,
+                    child: Center(child: CircularProgressIndicator()),
+                  ),
+                if (output != null)
+                  Padding(
+                    padding: horizontalPadding,
+                    child: SelectableText(
+                      output!,
+                      key: const Key("output"),
+                    ),
+                  ),
+                gap,
+                gap,
+              ],
+            ),
+          );
+        }),
       ),
     );
   }
