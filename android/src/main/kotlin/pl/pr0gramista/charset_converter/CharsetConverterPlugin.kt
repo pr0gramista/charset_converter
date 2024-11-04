8ba5922b64b7514d2be6e245139aa81776edaa66
Bartosz Wiśniewski <kontakt@pr0gramista.pl>
2024-11-04 12:01:34 +0100

    Fix Android build having fixed version

--- android/src/main/kotlin/pl/pr0gramista/charset_converter/CharsetConverterPlugin.kt
+++ android/src/main/kotlin/pl/pr0gramista/charset_converter/CharsetConverterPlugin.kt
@@ -1,40 +1,29 @@
 package pl.pr0gramista.charset_converter
 
-import androidx.annotation.NonNull;
+import androidx.annotation.NonNull
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
-import java.nio.charset.Charset
 import java.nio.ByteBuffer
+import java.nio.charset.Charset
 
 /** CharsetConverterPlugin */
-public class CharsetConverterPlugin: FlutterPlugin, MethodCallHandler {
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    val channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "charset_converter")
-    channel.setMethodCallHandler(CharsetConverterPlugin());
+class CharsetConverterPlugin: FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel : MethodChannel
+
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "charset_converter")
+    channel.setMethodCallHandler(this)
   }
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "charset_converter")
-      channel.setMethodCallHandler(CharsetConverterPlugin())
-    }
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+  override fun onMethodCall(call: MethodCall, result: Result) {
     if (call.method == "encode") {
       val buffer = Charset.forName(call.argument("charset")).encode(call.argument<String>("data"))
       val output = ByteArray(buffer.remaining());
@@ -58,6 +47,7 @@ public class CharsetConverterPlugin: FlutterPlugin, MethodCallHandler {
     }
   }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
   }
 }
